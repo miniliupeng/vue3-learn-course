a5d2e9ad5a32e406478ea03d3c128ff583085076
刘鹏 <2254842466@qq.com>
2024-10-18 16:01:15 +0800

    布局组件

--- component-lib/packages/components/main.ts
+++ component-lib/packages/components/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
 // import Example from './examples/theme.vue';
 // import Example from './examples/message.vue';
-import Example from './examples/dialog.vue';
+// import Example from './examples/dialog.vue';
+// import Example from './examples/gird.vue';
+import Example from './examples/layout.vue';
+
+
 import './src/index.less';
 
 const app = createApp(Example);
